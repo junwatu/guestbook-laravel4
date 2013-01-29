69b500b314e7e428dadb673f86f07731b98204d3
Equan Pr <eq@junwatu.com>
2013-01-29 20:12:13 +0700

    show guests list

--- app/views/guestslist.php
+++ app/views/guestslist.php
@@ -2,9 +2,25 @@
 <head>
     <title>Guests List</title>
     <meta charset="utf-8" lang="en">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <?php var_dump($data);?>
+<h4>Guests List | <a href="/">Home</a></h4>
+<table>
+    <tr>
+        <th>Name</th>
+        <th>Email</th>
+    </tr>
+    <?php
+
+    foreach ($data as $guest) {
+        echo "<tr>
+                <td>$guest->name</td>
+                <td>$guest->email</td>
+               </tr>";
+    };
+    ?>
+</table>
 </body>
 </html>
 
